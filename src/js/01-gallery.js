153244eef172bfc1f375996b2ec008fbc3a8708c
Maria-Kuka <mariakuka825@gmail.com>
2023-03-31 12:04:15 +0300

    update 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,76 @@
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createImageCardMarkup(galleryItems);np
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createImageCardMarkup(images) {
+  return images
+    .map(
+      ({ preview, original, description }) =>
+    `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    )
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+
+
+// import SimpleLightbox from 'simplelightbox';
+// import "simplelightbox/dist/simple-lightbox.min.css";
+// // Add imports above this line
+// import { galleryItems } from './gallery-items';
+// // Change code below this line
+// console.log(galleryItems);
+
+// const galleryContainer = document.querySelector('.gallery');
+// const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+// galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+
+// function createGalleryItemsMarkup(images) {
+//     return images.map(({ preview, original, description }) => 
+//  ` <a class="gallery__item" href="${original}">
+//     <img
+//       class="gallery__image"
+//       src="${preview}"
+//       alt="${description}"
+//     />
+//   </a>
+//   `
+//   ).join('');
+// }
+
+// new SimpleLightbox('.gallery a', {
+//   captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+// });
+// const galleryContainer = document.querySelector('.gallery');
+// const itemsMarkup = ({ preview, original, description }) => {
+//     return `
+//   <a class="gallery__item" href="${original}">
+//     <img
+//       class="gallery__image"
+//       src="${preview}"
+//       alt="${description}"
+//     />
+//   </a>
+// `;
+// };
+
+// // use library SimpleLightbox
+// const itemsMarkupList  = galleryItems.map(itemsMarkup).join('');
+// galleryContainer.insertAdjacentHTML('beforeend', itemsMarkupList);
+
